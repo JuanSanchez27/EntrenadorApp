b53bba7e981761c3e56a7109bff65b6015e41510
JuanSanchez27 <jdsanchez408@hotmail.com>
2021-08-09 15:31:45 -0500

    Se implementa el inicio de sesion del entrenador

---
+++ lib/LoginPage.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget{
+  static String id = 'LoginPage';
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 250.0,
+                  decoration: BoxDecoration(
+                    color: Colors.teal,
+                  ),
+                  child: SizedBox(
+                    child: Image.asset("assets/logo.png",
+                      cacheWidth: 220,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 520.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(200.0),
+                    ),
+                  ),
+                  child: Column(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Bienvenido",
+                        style: TextStyle(
+                          fontSize: 40.0,
+                        ),
+                      ),
+                      Text("Por favor inicie sesión para continuar",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      _textFieldUsername(),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      _textFieldPass(),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      Container(
+                        width: 300.0,
+                        child: ElevatedButton(
+                          onPressed: (){},
+                          child: Text("entrar"),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.teal.shade600,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+Widget _textFieldUsername() {
+  return Container(
+    width: 300.0,
+    child: TextFormField(
+        decoration: InputDecoration(
+            hintText: "correo/username",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(100.0),
+            )
+        )
+    ),
+  );
+}
+
+Widget _textFieldPass() {
+  return Container(
+    width: 300.0,
+    child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+            hintText: "contraseña",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(100.0),
+            )
+        )
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:entrenador_app/LoginPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,20 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Entrenador App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      initialRoute: LoginPage.id,
+      routes: {
+        LoginPage.id : (context) => LoginPage(),
+      },
     );
   }
 }
